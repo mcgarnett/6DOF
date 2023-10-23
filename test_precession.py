b7ea6684499666115869ea0163153f81eebd4ba9
Mark Garnett <mcgarnett@gmail.com>
2023-10-22 22:24:54 -0700

    cleaning up

--- test_precession.py
+++ test_precession.py
@@ -9,6 +9,9 @@ Created on Mon Aug 14 19:54:07 2023
 import numpy as np
 import openmdao.api as om
 import dymos as dm
+from scipy.spatial.transform import Rotation
+
+# from dm.visualization.timeseries_plots import timeseries_plots
 from trajectory.EOM import SixDOFGroup
 
 # p = om.Problem()
@@ -16,17 +19,17 @@ from trajectory.EOM import SixDOFGroup
 # p.setup()
 # om.n2(p)
 
-p = om.Problem(model=om.Group())
-
+# p = om.Problem(openmdao_reports_dir="reports/precession/")
+p = om.Problem()
 p.driver = om.pyOptSparseDriver()
 p.driver.options["optimizer"] = "IPOPT"
 p.driver.declare_coloring()
 
-p.driver.opt_settings["tol"] = 1.0e-5
-p.driver.opt_settings["print_level"] = 0
+# p.driver.opt_settings["tol"] = 1.0e-6
+p.driver.opt_settings["print_level"] = 1
 p.driver.opt_settings["mu_strategy"] = "monotone"
-p.driver.opt_settings["bound_mult_init_method"] = "mu-based"
-p.driver.opt_settings["mu_init"] = 0.01
+# p.driver.opt_settings["bound_mult_init_method"] = "mu-based"
+# p.driver.opt_settings["mu_init"] = 1
 
 
 t = dm.Radau(num_segments=10, order=3)
@@ -42,48 +45,62 @@ phase.set_time_options(fix_initial=True, duration_bounds=(1, 10), duration_ref=1
 
 # velocity in body frame coordinate system
 phase.add_state(
-    "V_bf",
+    "V_b",
+    # shape=(3,),
     fix_initial=True,
     units="m/s",
-    rate_source="V_rate",
-    # targets= V so we can get X_dot
+    rate_source="V_b_rate",
+    targets=["V_b"],
 )
 
 # angular velocity in body frame system
 phase.add_state(
-    "w_bf",
+    "w_b",
+    # shape=(3,),
     fix_initial=True,
     units="rad/s",
-    rate_source="w_rate",
-    # targets = a quaternion rotatiom thing
+    rate_source="w_b_rate",
+    targets=["w_b"],
 )
+
 
 # body frame to inertial frame rotation quaternion
 phase.add_state(
     "R",
     fix_initial=True,
-    units="1/s",
-    rate_source="R_dot",
-    # targets=["R"],
+    rate_source="R_rate",
+    targets=["R"],
 )
-
+I_mat = np.array([10, 0, 0], [0, 1, 0], [0, 0, 1])
+phase.add_parameter("F_by_m", units="m/s**2", targets=["F_by_m"])
+phase.add_parameter("M", units="N*m", targets=["M"])
+phase.add_parameter("I", val=I_mat, units="kg*m**2", targets=["I"])
 
 # test mixing wildcard ODE variable expansion and unit overrides
-# phase.add_timeseries_output(["aero.*", "prop.thrust", "prop.m_dot"], units={"aero.f_lift": "lbf", "prop.thrust": "lbf"})
+# phase.add_timeseries_output("*")
 
+phase.add_objective("time", loc="final", scaler=-1)
+# traj.add_parameter("F_by_m", val=np.zeros(3), units="m/s**2", opt=False)
+# traj.add_parameter("M", val=np.zeros(3), units="N*m", opt=False)
 
-p.model.linear_solver = om.DirectSolver()
-
+# p.model.linear_solver = om.DirectSolver()
+p.driver.declare_coloring()
 p.setup()
-
+p.set_val("traj.phase0.parameters:F_by_m", val=np.array([0, 0, -0.1]))
+p.set_val("traj.phase0.parameters:M", val=np.array([-0.1, 0, 0]))
 p["traj.phase0.t_initial"] = 0.0
-p["traj.phase0.t_duration"] = 350.0
+p["traj.phase0.t_duration"] = 10.0
 
-p["traj.phase0.states:r"] = phase.interp("r", [0.0, 111319.54])
-p["traj.phase0.states:h"] = phase.interp("h", [100.0, 20000.0])
-p["traj.phase0.states:v"] = phase.interp("v", [135.964, 283.159])
-p["traj.phase0.states:gam"] = phase.interp("gam", [0.0, 0.0])
-p["traj.phase0.states:m"] = phase.interp("m", [19030.468, 16841.431])
-p["traj.phase0.controls:alpha"] = phase.interp("alpha", [0.0, 0.0])
 
-dm.run_problem(p, simulate=True)
+p["traj.phase0.states:w_b"] = phase.interp("w_b", [[0.5, 0.5, 0], [0.5, 0.5, 0]])
+p["traj.phase0.states:V_b"] = phase.interp("V_b", [[0, 0, 1], [0, 0, 1]])
+rot = Rotation.from_euler("XYZ", [90, 0, 0], degrees=True)
+p["traj.phase0.states:R"] = np.roll(rot.as_quat(), 1)
+# p["traj.phase0.states:m"] = phase.interp("m", [19030.468, 16841.431])
+# p["traj.phase0.controls:alpha"] = phase.interp("alpha", [0.0, 0.0])
+
+# p.run_model()
+dm.run_problem(p, simulate=False, make_plots=False)
+# dm.visualization.timeseries_plots.timeseries_plots(simulation_record_file="precession_sim.db")
+# sim = p.model.traj.simulate(record_file="precession_sim.db")
+om.n2(p)
--- trajectory/EOM.py
+++ trajectory/EOM.py
@@ -24,18 +24,18 @@ class SixDOFGroup(om.Group):
         mat_vec_prod = om.MatrixVectorProductComp(
             A_name="I",
             A_units="kg*m**2",
-            x_name="w",
+            x_name="w_b",
             x_units="rad/s",
-            b_name="I_times_w",
+            b_name="I_times_w_b",
             b_units="kg*m**2/s",
             vec_size=nn,
             A_shape=(3, 3),
         )
 
         cross_prod_1 = om.CrossProductComp(
-            c_name="w_cross_V",
-            a_name="w",
-            b_name="V",
+            c_name="w_b_cross_V_b",
+            a_name="w_b",
+            b_name="V_b",
             c_units="m/s**2",
             a_units="rad/s",
             b_units="m/s",
@@ -43,9 +43,9 @@ class SixDOFGroup(om.Group):
         )
 
         cross_prod_1.add_product(
-            "w_cross_I_times_w",
-            a_name="w",
-            b_name="I_times_w",
+            "w_b_cross_I_times_w_b",
+            a_name="w_b",
+            b_name="I_times_w_b",
             c_units="N*m",
             a_units="rad/s",
             b_units="kg*m**2/s",
@@ -58,20 +58,20 @@ class SixDOFGroup(om.Group):
         adder = om.AddSubtractComp()
 
         adder.add_equation(
-            "V_rate",
-            ["F_by_m", "w_cross_V"],
+            "V_b_rate",
+            ["F_by_m", "w_b_cross_V_b"],
             vec_size=nn,
             length=3,
-            val=1.0,
+            val=0.0,
             units="m/s**2",
             scaling_factors=[1, -1],
         )
         adder.add_equation(
-            "w_rate_eqn_LHS",
-            ["M", "w_cross_I_times_w"],
+            "w_b_rate_eqn_LHS",
+            ["M", "w_b_cross_I_times_w_b"],
             vec_size=nn,
             length=3,
-            val=1.0,
+            val=0.0,
             units="N*m",
             scaling_factors=[1, -1],
         )
@@ -86,8 +86,8 @@ class SixDOFGroup(om.Group):
         self.add_subsystem(
             "body_rates",
             lin_sys,
-            promotes_inputs=[("A", "I"), ("b", "w_rate_eqn_LHS")],
-            promotes_outputs=[("x", "w_rate")],
+            promotes_inputs=[("A", "I"), ("b", "w_b_rate_eqn_LHS")],
+            promotes_outputs=[("x", "w_b_rate")],
         )
         self.add_subsystem(
             "quaternion_rates",
@@ -97,6 +97,8 @@ class SixDOFGroup(om.Group):
         )
 
         self.set_input_defaults("I", units="kg*m**2", val=np.tile(np.eye(3), [nn, 1, 1]))
+        self.set_input_defaults("F_by_m", units="m/s**2", val=np.zeros([nn, 3]))
+        self.set_input_defaults("M", units="N*m", val=np.zeros([nn, 3]))
 
 
 class QuaternionRates(om.ExplicitComponent):
@@ -107,10 +109,10 @@ class QuaternionRates(om.ExplicitComponent):
         nn = self.options["num_nodes"]
 
         self.add_input("R", val=np.ones([nn, 4]), desc="Rotation Quaternion")
-        self.add_input("w", val=np.ones([nn, 3]), units="rad/s", desc="body angular rates")
-        self.add_output("R_dot", val=np.zeros([nn, 4]), desc="Rotation Quaternion rates", units="1/s")
+        self.add_input("w_b", val=np.ones([nn, 3]), units="rad/s", desc="body angular rates")
+        self.add_output("R_rate", val=np.zeros([nn, 4]), desc="Rotation Quaternion rates", units="1/s")
 
-        # permutation matrix
+        # permutation matrices to get quaternion multiplication
         self.P = np.array(
             [
                 [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, -1]],
@@ -133,7 +135,7 @@ class QuaternionRates(om.ExplicitComponent):
                     cols += [4 * k + j]
         # print(rows)
         # print(cols)
-        self.declare_partials("R_dot", "R", rows=rows, cols=cols)
+        self.declare_partials("R_rate", "R", rows=rows, cols=cols)
 
         range3 = np.arange(3)
         rows = []
@@ -144,32 +146,18 @@ class QuaternionRates(om.ExplicitComponent):
                     rows += [4 * k + i]
                     cols += [3 * k + j]
 
-        self.declare_partials("R_dot", "w", rows=rows, cols=cols)
+        self.declare_partials("R_rate", "w_b", rows=rows, cols=cols)
 
     def compute(self, inputs, outputs):
         nn = self.options["num_nodes"]
         R = inputs["R"]
-        omega = inputs["w"]
+        omega = inputs["w_b"]
 
         omega_quat = np.column_stack((np.zeros(nn), omega))
 
         # R_w, R_x, R_y, R_z = R[:, 0], R[:, 1], R[:, 2], R[:, 3]
 
         # om_x, om_y, om_z = omega[:, 0], omega[:, 1], omega[:, 2]
-
-        # R is 1 w is 2
-        # w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
-        # x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
-        # y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
-        # z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
-
-        # R_dot = np.zeros([nn, 4])
-        # om_w = 0
-        # orignal
-        # R_dot[:, 0] = R_w * om_w - R_x * om_x - R_y * om_y - R_z * om_z
-        # R_dot[:, 1] = R_w * om_x + R_x * om_w + R_y * om_z - R_z * om_y
-        # R_dot[:, 2] = R_w * om_y + R_y * om_w + R_z * om_x - R_x * om_z
-        # R_dot[:, 3] = R_w * om_z + R_z * om_w + R_x * om_y - R_y * om_x
 
         # omega ordered
         # R_dot[:, 0] = R_w * om_w - R_x * om_x - R_y * om_y - R_z * om_z
@@ -183,30 +171,21 @@ class QuaternionRates(om.ExplicitComponent):
         # R_dot[:, 2] = R_w * om_y - R_x * om_z + R_y * om_w + R_z * om_x
         # R_dot[:, 3] = R_w * om_z + R_x * om_y - R_y * om_x + R_z * om_w
 
-        # R_dot = np.einsum("jkl,k->jl",w_shuffle,t)
-        # np.einsum("jkl,ik->ijl",w_shuffle,om)
-        # print(R)
-        # R_dot2 = np.einsum("il,jkl,ik->ij", R, self.R_permute, omega_quat)
-        R_dot2 = np.einsum("ij,ljk,ik->il", R, self.P, omega_quat)
-        # print(R_dot - R_dot2)
-        # print(R_dot)
-        # print(R_dot2)
-        # print(R_dot[:, 0])
-        # print(R_w, R_x, R_y, R_z)
+        R_rate = 0.5 * np.einsum("ij,ljk,ik->il", R, self.P, omega_quat)
 
-        outputs["R_dot"] = R_dot2
+        outputs["R_rate"] = R_rate
 
     def compute_partials(self, inputs, J):
         nn = self.options["num_nodes"]
         R = inputs["R"]
-        omega = inputs["w"]
+        omega = inputs["w_b"]
         omega_quat = np.column_stack((np.zeros(nn), omega))
 
         # magic formulas. Taking partials involves not dropping the index over which you sum in the formula
 
-        J["R_dot", "R"] = np.einsum("ljk,ik->ilj", self.P, omega_quat).flatten()
+        J["R_rate", "R"] = 0.5 * np.einsum("ljk,ik->ilj", self.P, omega_quat).flatten()
 
-        J["R_dot", "w"] = np.einsum("ljk,ij->ilk", self.P_reduced, R).flatten()
+        J["R_rate", "w_b"] = 0.5 * np.einsum("ljk,ij->ilk", self.P_reduced, R).flatten()
 
 
 class DownVector(om.ExplicitComponent):
@@ -240,8 +219,8 @@ if __name__ == "__main__":
     p = om.Problem()
     p.model.add_subsystem("six_dof", SixDOFGroup(num_nodes=2))
     p.setup(force_alloc_complex=True)
-    p.set_val("six_dof.V", val=np.array([1, 10, 100]), units="m/s")
-    p.set_val("six_dof.w", val=np.array([2, 3, 4]), units="rad/s")
+    p.set_val("six_dof.V_b", val=np.array([1, 10, 100]), units="m/s")
+    p.set_val("six_dof.w_b", val=np.array([2, 3, 4]), units="rad/s")
     p.set_val("six_dof.R", val=np.array([1, 2, 3, 4]))
     p.set_val("six_dof.F_by_m", val=np.array([10, 0, 0]), units="m/s**2")
     p.set_val("six_dof.M", val=np.array([1, -1, 1]), units="N*m")
